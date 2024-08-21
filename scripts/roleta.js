8891942e83d7af00ae418a6f1be20996eff240cc
Noname <vinimoura340@gmail.com>
2024-08-21 18:40:34 -0300

    Objetos

--- scripts/roleta.js
+++ scripts/roleta.js
@@ -1,11 +1,42 @@
 
-var jogos = ["Roblox", "Minecraft", "Terraria"];
+var jogos = [
 
-    function sorteado() {
-       return jogos[Math.floor(Math.random() * jogos.length)];
-    }
+{
+nome: 'Valorant',
+tipo: 'FPS',
+ano: 2020,
+descricao: function() {
+return `${this.nome} é um jogo de ${this.tipo} lançado em ${this.ano}.`;
+}
+},
+   
+{
+nome: 'Minecraftt',
+tipo: 'Sandbox',
+ano: 2011,
+descricao: function() {
+   return `${this.nome} é um jogo de ${this.tipo} lançado em ${this.ano}.`;
+}
+},
 
-    function abcde() {
-   console.log(sorteado());
+      
+{
+nome: 'Roblox',
+tipo: 'Sandbox',
+ano: 2006,
+descricao: function() {
+   return `${this.nome} é um jogo de ${this.tipo} lançado em ${this.ano}.`;
+}
+},
+];
+
+   function abcde() { 
+   const jogo = (function() {  
+      return jogos[Math.floor(Math.random() * jogos.length)];
+ })();
+
+
+   console.log(jogo);
+   console.log(jogo.descricao());
 }
 /* parte de cima já é a roleta */
