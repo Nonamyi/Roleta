86fd4b0c1b7b56d98620bc91e2e3eaed60f32bf0
Noname <vinimoura340@gmail.com>
2024-09-19 20:12:37 -0300

    forEach

--- scripts/roleta.js
+++ scripts/roleta.js
@@ -2,7 +2,7 @@ var clicks = 0;
 
 function contarClicks() { // usado para que no final os cicks ssejam atualizados 
     clicks++; // cada click conta +1 função
-    console.log("O número de vezes foi: " + clicks); 
+    console.log("O número total de vezes foi: " + clicks); 
 }
 
 var jogos = [
@@ -26,7 +26,7 @@ var jogos = [
     }
 ];
 
-function abcde() { 
+function abcde() {{ // { a mais pra fechar com o } no final do codigo pra integrar o forEach e retornar ele inteiro novamente atualizado
     const jogo = (function() {  
         return jogos[Math.floor(Math.random() * jogos.length)];
     })(); // função abcde ( click ), chama o jogo ( array ) com a função de retornar e multiplicar pelo array
@@ -34,6 +34,10 @@ function abcde() {
     jogo.clicks++; // "reconta" a função clicks do jogo pra contar +1 quando foi selecionado e mossra o valor
     contarClicks();  // clicks atualizados
     console.log(jogo); // c.l no jogo
-    console.log(`Clique(s) no jogo ${jogo.nome}: ${jogo.clicks} vez(es).`); // c.l mostrando cliques no jogo e nome
+    console.log(`Clique(s) no jogo ${jogo.nome}: ${jogo.clicks} vezes.`); // c.l mostrando cliques no jogo e nome
 }
-   
+
+jogos.forEach(function(jogo2) { // jogo2 "chama" o conteudo do array(jogos) --> jogo é o sorteado e o jogo2 "representa" cada jogo
+    console.log(`Nome: ${jogo2.nome}, Clique(s): ${jogo2.clicks} vezes.`);
+});
+}
